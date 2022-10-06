948fccb8f149a18c38726f59872c12efdb173fe9
Abhishek <takawaneabhishek04@gmail.com>
2022-10-06 17:58:21 +0530

    first commit

---
+++ ff4.js
@@ -0,0 +1,13 @@
+var flower = new Array();
+flower[0] =new Array(1,2,3)
+flower[0] =new Array(1,2,3)
+Document.write(flower[0][0]);
+
+let text = "";
+let i = 0;
+do {
+  text += i + "<br>";
+  i++;
+}
+while (i < 5);
+document.getElementById("demo").innerHTML = text;
